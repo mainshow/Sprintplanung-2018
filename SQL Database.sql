79879af56193689f53a7f875eb7d5c33783c8f48
mainshow <tom-muschkiet@gmx.de>
2018-04-12 19:01:29 +0200

    SQL Systematik hinzugefügt

--- SQL Database.sql
+++ SQL Database.sql
@@ -1,9 +1,11 @@
-create database sprintplanung;
+#create database sprintplanung;
 
 use sprintplanung;
 
-CREATE USER sqluser IDENTIFIED BY 'sqluserpw';
-grant all privileges  on sprintplanung.* to sqluser@localhost;
+#CREATE USER sqluser IDENTIFIED BY 'sqluserpw';
+
+#grant usage on *.* to sqluser@localhost identified by 'sqluserpw';
+#grant all privileges  on sprintplanung.* to sqluser@localhost;
 
 CREATE TABLE aufgabe (
 id INT NOT NULL AUTO_INCREMENT,
@@ -12,5 +14,7 @@ anlagedatum DATE NOT NULL,
 standardaufgabe BOOLEAN,
 aktuellerstatus VARCHAR(10),
 geschätzteraufwand INT NOT NULL,
-realeraufwand INT NOT NULL
-)
+realeraufwand INT NOT NULL,
+kommentar VARCHAR(180),
+PRIMARY KEY (id)
+);
